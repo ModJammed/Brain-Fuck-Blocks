d58b68e2f3839bcc91ff34a71b7c621b0e7251f8
Dylan Miller <dmillerw@gmail.com>
2013-03-29 08:26:37 -0700

    Back. Some minor work on wire render

--- bf_common/com/dmillerw/brainFuckBlocks/block/BlockWire.java
+++ bf_common/com/dmillerw/brainFuckBlocks/block/BlockWire.java
@@ -1,17 +1,32 @@
 package com.dmillerw.brainFuckBlocks.block;
 
-import com.dmillerw.brainFuckBlocks.BrainFuckBlocks;
-
 import net.minecraft.block.BlockContainer;
 import net.minecraft.block.material.Material;
+import net.minecraft.client.renderer.texture.IconRegister;
 import net.minecraft.tileentity.TileEntity;
+import net.minecraft.util.Icon;
+import net.minecraft.world.IBlockAccess;
 import net.minecraft.world.World;
+import net.minecraftforge.common.ForgeDirection;
+
+import com.dmillerw.brainFuckBlocks.BrainFuckBlocks;
+import com.dmillerw.brainFuckBlocks.lib.ModInfo;
 
 public class BlockWire extends BlockContainer {
 
+	private Icon defaultTexture;
+	private Icon inputTexture;
+	private Icon outputTexture;
+	
 	public BlockWire(int id) {
 		super(id, Material.cloth);
 		setCreativeTab(BrainFuckBlocks.creativeTabBF);
+	}
+	
+	@Override
+	public Icon getBlockTexture(IBlockAccess world, int x, int y, int z, int side) {
+		//TODO Interaction with TE
+		return defaultTexture;
 	}
 	
 	@Override
@@ -27,6 +42,13 @@ public class BlockWire extends BlockContainer {
 	@Override
 	public boolean renderAsNormalBlock() {
 		return false;
+	}
+	
+	@Override
+	public void registerIcons(IconRegister register) {
+		defaultTexture = register.registerIcon(ModInfo.MOD_ID.toLowerCase()+":wire_side");
+		inputTexture = register.registerIcon(ModInfo.MOD_ID.toLowerCase()+":wire_in");
+		outputTexture = register.registerIcon(ModInfo.MOD_ID.toLowerCase()+":wire_out");
 	}
 	
 	@Override
