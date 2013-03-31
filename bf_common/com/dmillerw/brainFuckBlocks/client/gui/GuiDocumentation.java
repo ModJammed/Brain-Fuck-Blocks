379e4da4d3e147ccd4c78fbe7dee5c902d4c2a95
Dylan Miller <dmillerw@gmail.com>
2013-03-31 10:26:29 -0700

    Auto-commit 2013-03-31 10:26:29

--- bf_common/com/dmillerw/brainFuckBlocks/client/gui/GuiDocumentation.java
+++ bf_common/com/dmillerw/brainFuckBlocks/client/gui/GuiDocumentation.java
@@ -16,14 +16,14 @@ public class GuiDocumentation extends GuiScreen {
 	private FontRenderer font = FMLClientHandler.instance().getClient().fontRenderer;
 	
 	public void drawCenteredString(String text, int yPos, int color) {
-		
+		font.drawString(text, (GuiUtils.getScreenWidth() / 2) - (font.getStringWidth(text) / 2), yPos, color);
 	}
 	
 	public void render() {
 		GuiUtils.disableDefaults();
 		GuiUtils.renderRect(xSub, ySub, GuiUtils.getScreenWidth() - xSub, GuiUtils.getScreenHeight() - ySub, 0, 0, 0, 122);
 		GL11.glScalef(2F, 2F, 2F);
-		font.drawStringWithShadow("Hello", 0, 0, 0xFFFFFF);
+		drawCenteredString("Hello", 100, 0x000000);
 		GL11.glScalef(.5F, .5F, .5F);
 		GuiUtils.enableDefaults();
 	}
