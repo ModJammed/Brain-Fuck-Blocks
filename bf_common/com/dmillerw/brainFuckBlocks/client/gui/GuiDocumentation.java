b4c736ff30fd88a5dd48f32a563fc8851dcc0570
Dylan Miller <dmillerw@gmail.com>
2013-03-31 10:11:15 -0700

    Auto-commit 2013-03-31 10:11:15

--- bf_common/com/dmillerw/brainFuckBlocks/client/gui/GuiDocumentation.java
+++ bf_common/com/dmillerw/brainFuckBlocks/client/gui/GuiDocumentation.java
@@ -1,6 +1,8 @@
 package com.dmillerw.brainFuckBlocks.client.gui;
 
+import cpw.mods.fml.client.FMLClientHandler;
 import kovu.utils.gui.GuiUtils;
+import net.minecraft.client.gui.FontRenderer;
 import net.minecraft.client.gui.GuiScreen;
 
 public class GuiDocumentation extends GuiScreen {
@@ -8,7 +10,14 @@ public class GuiDocumentation extends GuiScreen {
 	public int page = 1;
 	
 	public void render() {
-		GuiUtils.drawRect(0, 0, 500, 500, 0x000000);
+		GuiUtils.disableDefaults();
+		GuiUtils.renderRect(0, 0, GuiUtils.getScreenWidth(), GuiUtils.getScreenHeight(), 0, 0, 0, 122);
+		
+		FontRenderer font = FMLClientHandler.instance().getClient().fontRenderer;
+		
+		font.drawStringWithShadow("Hello", 0, 0, 0x000000);
+		
+		GuiUtils.enableDefaults();
 	}
 	
 }
