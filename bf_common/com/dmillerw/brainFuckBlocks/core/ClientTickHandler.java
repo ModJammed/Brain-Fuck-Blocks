66e08164ca1e622a180e27f1daedd1ddd8add915
Dylan Miller <dmillerw@gmail.com>
2013-03-31 09:31:07 -0700

    Scripted auto-commit on change (2013-03-31 09:31:07) by gitwatch.sh

--- bf_common/com/dmillerw/brainFuckBlocks/core/ClientTickHandler.java
+++ bf_common/com/dmillerw/brainFuckBlocks/core/ClientTickHandler.java
@@ -1,7 +1,30 @@
 package com.dmillerw.brainFuckBlocks.core;
 
+import java.util.EnumSet;
+
 import cpw.mods.fml.common.ITickHandler;
+import cpw.mods.fml.common.TickType;
 
 public class ClientTickHandler implements ITickHandler {
+
+	@Override
+	public void tickStart(EnumSet<TickType> type, Object... tickData) {
+		
+	}
+
+	@Override
+	public void tickEnd(EnumSet<TickType> type, Object... tickData) {
+		
+	}
+
+	@Override
+	public EnumSet<TickType> ticks() {
+		return null;
+	}
+
+	@Override
+	public String getLabel() {
+		return null;
+	}
 
 }
