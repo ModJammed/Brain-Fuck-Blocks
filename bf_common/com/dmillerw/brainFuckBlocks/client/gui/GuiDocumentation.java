8ec2cfd97952b3b39945b316336d29dbcf9d216f
Dylan Miller <dmillerw@gmail.com>
2013-03-31 10:21:23 -0700

    Auto-commit 2013-03-31 10:21:23

--- bf_common/com/dmillerw/brainFuckBlocks/client/gui/GuiDocumentation.java
+++ bf_common/com/dmillerw/brainFuckBlocks/client/gui/GuiDocumentation.java
@@ -10,17 +10,21 @@ import net.minecraft.client.gui.GuiScreen;
 public class GuiDocumentation extends GuiScreen {
 
 	public int page = 1;
+	public int xSub = (int) (GuiUtils.getScreenWidth() * 0.8);
+	public int ySub = (int) (GuiUtils.getScreenHeight() * 0.8);
+	
+	private FontRenderer font = FMLClientHandler.instance().getClient().fontRenderer;
+	
+	public void drawCenteredString(String text, int yPos, int color) {
+		
+	}
 	
 	public void render() {
 		GuiUtils.disableDefaults();
-		GuiUtils.renderRect(0, 0, GuiUtils.getScreenWidth(), GuiUtils.getScreenHeight(), 0, 0, 0, 122);
-		
-		FontRenderer font = FMLClientHandler.instance().getClient().fontRenderer;
-		
+		GuiUtils.renderRect(xSub, ySub, GuiUtils.getScreenWidth() - xSub, GuiUtils.getScreenHeight() - ySub, 0, 0, 0, 122);
 		GL11.glScalef(2F, 2F, 2F);
 		font.drawStringWithShadow("Hello", 0, 0, 0xFFFFFF);
 		GL11.glScalef(.5F, .5F, .5F);
-		
 		GuiUtils.enableDefaults();
 	}
 	
