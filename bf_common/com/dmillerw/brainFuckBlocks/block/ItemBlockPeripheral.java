d916af16bffd841a59958f62c0144113902a337a
Dylan Miller <dmillerw@gmail.com>
2013-04-01 09:41:55 -0700

    Added tooltips for peripherals to show whether it's input or output

--- bf_common/com/dmillerw/brainFuckBlocks/block/ItemBlockPeripheral.java
+++ bf_common/com/dmillerw/brainFuckBlocks/block/ItemBlockPeripheral.java
@@ -1,11 +1,15 @@
 package com.dmillerw.brainFuckBlocks.block;
 
+import java.util.List;
+
+import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.item.ItemBlock;
 import net.minecraft.item.ItemStack;
 
 public class ItemBlockPeripheral extends ItemBlock {
 
 	private static String[] subNames = new String[] {"redstoneData", "redstoneInput", "chatData"};
+	private static String[] ioTypes = new String[] {"Output", "Input", "Output"};
 	
 	public ItemBlockPeripheral(int id) {
 		super(id);
@@ -13,6 +17,11 @@ public class ItemBlockPeripheral extends ItemBlock {
 		setHasSubtypes(true);
 	}
 
+	@SuppressWarnings({ "unchecked", "rawtypes" })
+	public void addInformation(ItemStack stack, EntityPlayer player, List list, boolean show) {
+		list.add(ioTypes[stack.getItemDamage()]);
+	}
+	
 	@Override
 	public int getMetadata(int damage) {
 		return damage;
