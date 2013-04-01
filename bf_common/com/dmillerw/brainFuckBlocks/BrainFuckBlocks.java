aaec6f99144eb971db37806f09d84b37ed9c694a
Dylan Miller <dmillerw@gmail.com>
2013-03-31 23:14:20 -0700

    FINAL COMMIT

--- bf_common/com/dmillerw/brainFuckBlocks/BrainFuckBlocks.java
+++ bf_common/com/dmillerw/brainFuckBlocks/BrainFuckBlocks.java
@@ -108,7 +108,11 @@ public class BrainFuckBlocks {
 		//CPU
 		GameRegistry.addRecipe(new ItemStack(BlockHandler.bfCPU), new Object[] {"T", "A", "M", 'T', new ItemStack(ItemHandler.bfCraftingComponent, 1, 4), 'A', new ItemStack(ItemHandler.bfCraftingComponent, 1, 3), 'M', new ItemStack(BlockHandler.bfCode, 1, 8)});
 		//Wire
-		GameRegistry.addRecipe(new ItemStack(BlockHandler.bfWire, new Object[] {" B ", "BRB", " B ", ))
+		GameRegistry.addRecipe(new ItemStack(BlockHandler.bfWire), new Object[] {" B ", "BRB", " B ", 'B', new ItemStack(Block.cloth, 1, 15), 'R', Item.redstone});
+		//Redstone Data Interpreter
+		GameRegistry.addRecipe(new ItemStack(BlockHandler.bfPeripheralRS, 1, 0), new Object[] {"R", "M", 'R', Block.blockRedstone, 'M', new ItemStack(BlockHandler.bfCode, 1, 4)});
+		//Redstone Input Interpreter
+		GameRegistry.addRecipe(new ItemStack(BlockHandler.bfPeripheralRS, 1, 1), new Object[] {"R", "M", 'R', Block.blockRedstone, 'M', new ItemStack(BlockHandler.bfCode, 1, 5)});
 		
 		//Code blocks
 		if (UserPreferences.codeBlockCraftingEnable) {
