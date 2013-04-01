5cfcca7b21736f53f6b631f3dcd4ddce24d20350
Dylan Miller <dmillerw@gmail.com>
2013-03-31 20:10:12 -0700

    Auto-commit 2013-03-31 20:10:12

---
+++ bf_common/com/dmillerw/brainFuckBlocks/client/gui/GuiDocumentation.java
@@ -0,0 +1,23 @@
+package com.dmillerw.brainFuckBlocks.client.gui;
+
+import org.lwjgl.opengl.GL11;
+
+import net.minecraft.client.gui.GuiScreen;
+
+public class GuiDocumentation extends GuiScreen {
+
+    private int bookImageWidth = 192;
+    private int bookImageHeight = 192;
+	
+	public void drawScreen(int par1, int par2, float par3)
+    {
+        GL11.glColor4f(1.0F, 1.0F, 1.0F, 1.0F);
+        this.mc.renderEngine.bindTexture("/gui/book.png");
+        int k = (this.width - this.bookImageWidth) / 2;
+        byte b0 = 2;
+        this.drawTexturedModalRect(k, b0, 0, 0, this.bookImageWidth, this.bookImageHeight);
+
+        super.drawScreen(par1, par2, par3);
+    }
+	
+}
